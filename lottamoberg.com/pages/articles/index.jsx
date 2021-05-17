7fa84617600042ea682fcc26ecd83ca5db2e6b71
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-05-16 23:28:06 -0500

    :sparkles: add basic endpoint index files

---
+++ lottamoberg.com/pages/articles/index.jsx
@@ -0,0 +1,44 @@
+import Intro from '../../components/Intro/Intro';
+import HeroPost from '../../components/Posts/HeroPost';
+import MoreStories from '../../components/Posts/MoreStories';
+import {getAllPostsForHome} from '../../lib/api';
+
+
+export default function BlogPage({allPosts, }) {
+  if (!allPosts) return null;
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  console.log(JSON.stringify(allPosts));
+  return (
+    <>
+      <div className="pt-24" />
+      <div className="container mx-auto px-5">
+        <Intro />
+        <div className="pt-24" />
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            coverImage={heroPost.mainImage}
+            date={heroPost.date}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+            postTypePath='articles'
+          />
+        )}
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps({preview = false}) {
+  /* Article Content */
+  const allPosts = await getAllPostsForHome(preview, 'article');
+  console.log(preview)
+  return {
+    props: {
+      allPosts: allPosts,
+      preview: preview,
+    },
+  };
+}
---
+++ lottamoberg.com/pages/research/index.jsx
@@ -0,0 +1,42 @@
+import Intro from '../../components/Intro/Intro';
+import HeroPost from '../../components/Posts/HeroPost';
+import MoreStories from '../../components/Posts/MoreStories';
+import {getAllPostsForHome} from '../../lib/api';
+
+
+export default function BlogPage({allPosts, }) {
+  if (!allPosts) return null;
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <div className="pt-24" />
+      <div className="container mx-auto px-5">
+        <Intro />
+        <div className="pt-24" />
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            coverImage={heroPost.mainImage}
+            date={heroPost.date}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+            postTypePath='research'
+          />
+        )}
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps({preview = false}) {
+  /* Research Content */
+  const allPosts = await getAllPostsForHome(preview, 'research');
+  return {
+    props: {
+      allPosts: allPosts,
+      preview: preview,
+    },
+  };
+}
