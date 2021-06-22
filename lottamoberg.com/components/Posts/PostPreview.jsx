0f69f3567128b074300f0b3f749a97d251baa0f3
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-06-22 17:00:36 -0500

    :beetle: :recycle: fix dynamic route bug with postName prop

--- lottamoberg.com/components/Posts/PostPreview.jsx
+++ lottamoberg.com/components/Posts/PostPreview.jsx
@@ -8,15 +8,16 @@ export default function PostPreview({
   date,
   excerpt,
   slug,
+  postName = 'blog', // 'research' | 'article'
 }) {
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} url={coverImage} />
+        <CoverImage slug={slug} title={title} url={coverImage} postTypePath={postName} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a href={`/posts/${slug}`} className="hover:underline">
+        <Link as={`/${postName}/${slug}`} href={`/${postName}/[slug]`}>
+          <a href={`/${postName}/${slug}`} className="hover:underline">
             {title}
           </a>
         </Link>
