e81c4a7b2748f398a1703aa275495ef307325d1b
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-06-23 10:26:34 -0500

    :see_no_evil: banish nextjs eslint usage within the build process

--- lottamoberg.com/next.config.js
+++ lottamoberg.com/next.config.js
@@ -3,6 +3,11 @@
 module.exports = {
   images: {domains: ['res.cloudinary.com']},
   target: 'serverless',
+  eslint: {
+    // Warning: Allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.(png|jpg|gif|svg|eot|ttf|woff2|woff)$/,
