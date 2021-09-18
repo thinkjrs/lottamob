31ae0d97de32193e3962e4cd7ec7f3f9a1f6d2d9
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-09-18 12:13:30 -0500

    :sparkles: add svg auto-image creation

--- lottamoberg.com/components/Posts/CoverImage.jsx
+++ lottamoberg.com/components/Posts/CoverImage.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { urlForImage } from '../../lib/sanity';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 function Image({ src, title }) {
   return (
@@ -12,18 +13,42 @@ function Image({ src, title }) {
     />
   );
 }
+
 export default function CoverImage({
   title,
   url,
   slug,
   postTypePath = 'blog',
+  hero = false,
 }) {
   const imgLink = `/${postTypePath}/${slug}`;
   return (
-    <div className="-mx-5 sm:mx-0">
+    <div className="-mx-5 sm:mx-0 flex">
       <Link as={`/${postTypePath}/${slug}`} href={`/${postTypePath}/[slug]`}>
-        <a aria-label={title} href={imgLink}>
-          <Image src={url} title={title} />
+        <a className="" aria-label={title} href={imgLink}>
+          {hero ? (
+            <div
+              className="antialiased flex items-center w-auto text-center text-gray-50 text-3xl "
+              id="background-pattern-container"
+              style={{
+                backgroundImage: `url("./protruding-squares.svg")`,
+                minHeight: '27rem',
+              }}
+            >
+              {title}
+            </div>
+          ) : (
+            <div
+              className="antialiased flex flex-wrap w-auto items-center text-center text-white text-2xl font-semibold tracking-wide"
+              id="background-pattern-container"
+              style={{
+                backgroundImage: `url("./large-triangles.svg")`,
+                minHeight: '12.45rem',
+              }}
+            >
+              <div className="p-1">{title}</div>
+            </div>
+          )}
         </a>
       </Link>
     </div>
