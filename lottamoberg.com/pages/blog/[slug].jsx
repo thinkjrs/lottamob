695f05176810c0ea8f5f4f53a100dea55c1253fd
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-09-19 10:42:19 -0500

    :recycle: add naive generalized blog pots section

--- lottamoberg.com/pages/blog/[slug].jsx
+++ lottamoberg.com/pages/blog/[slug].jsx
@@ -1,15 +1,14 @@
 // Dynamic, build-time blog page population by `id` in getStaticProps
 // and getStaticPaths for server-side-rendering.
-import Head from 'next/head';
-
 import SectionSeparator from '../../components/Layout/SectionSeparator';
 import MoreStories from '../../components/Posts/MoreStories';
 import { PostBody, PostHeader } from '../../components/Posts/Posts';
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api';
 
 function ArticleMetaOG({ post, ogUrl }) {
+  const date = new Date();
   return (
-    <Head>
+    <>
       <title>{post?.title}</title>
       <meta property="og:title" content={post?.title} />
       <meta property="og:type" content="article" />
@@ -17,37 +16,41 @@ function ArticleMetaOG({ post, ogUrl }) {
       <meta property="article:section" content={`${post?.title} - Moberg`} />
 
       <meta property="article:published_time" content={post?.date} />
-      <meta property="og:image" content={post?.ogImag} />
+      <meta property="og:image" content={post?.mainImage} />
       <meta
         property="og:description"
-        content={`${post?.title} by Lotta Moberg, PhD, CFA on ${post?.date}.`}
+        content={`${post?.title} by Lotta Moberg, PhD, CFA on ${
+          post?.date || date
+        }.`}
       />
-    </Head>
+    </>
   );
 }
 export default function BlogPost({ post, morePosts, preview }) {
-  const coverImage = post?.mainImage;
   return (
     <>
       <div className="pt-20" />
       <div className="container mx-auto px-5">
-        <article>
+        <article id="content-start">
           <ArticleMetaOG post={post} ogUrl="/" />
           <PostHeader
             title={post?.title}
             coverImage={post?.mainImage}
             date={post?.date}
+            postName="blog"
           />
           <PostBody content={post?.body} />
         </article>
         <SectionSeparator />{' '}
-        {morePosts?.length > 0 && <MoreStories posts={morePosts} />}
+        {morePosts?.length > 0 && (
+          <MoreStories posts={morePosts} postName="blog" />
+        )}
       </div>
     </>
   );
 }
 export async function getStaticProps({ params, preview = false }) {
-  const posts = await getPostAndMorePosts(params.slug, preview);
+  const posts = await getPostAndMorePosts(params.slug, preview, 'post');
   // query the data to render for posts
   return {
     props: {
