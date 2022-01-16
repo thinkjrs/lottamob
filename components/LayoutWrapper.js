1a5fd5b4a9ecd8d05335215aa9cc82ad37557a2c
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-01-16 17:35:26 -0600

    :sparkles: add headshot logo [sc-21]

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,13 +1,14 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import Image from 'next/image'
 
 const LayoutWrapper = ({ children }) => {
+  const logo = '/static/images/lotta-headshot.jpeg'
   return (
     <SectionContainer>
       <div className="flex flex-col justify-between h-screen">
@@ -15,8 +16,8 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label="Tailwind CSS Blog">
               <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
+                <div className="mr-3 rounded-full">
+                  <Image className="rounded-full" src={logo} width="80" height="80" priority />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
