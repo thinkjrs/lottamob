065a3dc40d78eb1464e6289e645faeb0c03a9ae5
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-01-16 20:48:37 -0600

    :recycle: :wrench: add seo items to app [sc-21]

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
-
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
@@ -17,6 +16,8 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="title" content={siteMetadata?.title} />
+        <meta name="description" content={siteMetadata?.description} />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
