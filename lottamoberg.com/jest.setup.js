e028c8ab8d18113e674633e098168fde5c1cc4b5
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-03-15 17:37:59 -0500

    :fire: remove old site

--- lottamoberg.com/jest.setup.js
+++
@@ -1,3 +0,0 @@
-/* Let's be honest. Jest + React testing ecosystem is a fucking abomination. This file is for things that jest needs because of its and JavaScript's various attrocities are outlandishly damaging to global productivity. */
-NEXT_PUBLIC_SANITY_DATASET = "production"
-NEXT_PUBLIC_SANITY_PROJECT_ID = "6zq2ow72"
