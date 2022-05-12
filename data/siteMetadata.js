269b787d2f31c56f2d91d40d6231397c528e7581
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-05-12 16:09:57 -0500

    :recycle: switch description to ViviFi Ventures

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   author: 'Lotta Moberg, PhD, CFA',
   headerTitle: '',
   description:
-    'My research and analysis synthesizes global economics, politics, and policy as a macroeconomic analyst at the Dynamic Allocations Strategies team at William Blair.',
+    'My research and analysis synthesizes global economics, politics, and policy. I am a Founder am co-CIO of ViviFi Ventures',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://lottamoberg.com',
